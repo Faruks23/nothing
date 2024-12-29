11af3d8271cf96eafdf1492c9826ad299aa6e640
Faruks23 <mdfaruksp2@gmail.com>
2024-12-29 10:16:42 +0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import AccountManager from "./components/AccountManager";
 export default function Home() {
   return (
     <main>
-      <h1>Facebook Account Manager</h1>
+     
       <AccountManager />
     </main>
    
